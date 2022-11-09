0b981d0bad479b5417931811329b14168e84f259
Carlos Carmona Castillo <carlos.carmona@bosonit.com>
2022-11-09 16:17:58 +0100

    realizado

--- 08-lifecycle/src/app/app.module.ts
+++ 08-lifecycle/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MuestraNombreComponent } from './components/muestra-nombre/muestra-nombre.component';
+import { Pagina1Component } from './pages/pagina1/pagina1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MuestraNombreComponent,
+    Pagina1Component,
   ],
   imports: [
     BrowserModule
